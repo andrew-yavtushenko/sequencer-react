2a8194166784ce52711287e3f83f066e5bbb3d25
Andrey Yavtushenko <andrew.yavtushenko@gmail.com>
2015-10-10 16:17:41 +0300

    gitignore

---
+++ src/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require.context('./worker/', true, /\.js$/);
+
+var SequencerReactApp = require('./containers/SequencerReactApp');
+var React = require('react');
+var { Provider } = require('react-redux');
+var configureStore = require('./store/configureStore');
+var store = configureStore();
+
+React.render(
+  <Provider store={store}>
+    {() => <SequencerReactApp />}
+  </Provider>,
+  document.getElementById('content')
+);
